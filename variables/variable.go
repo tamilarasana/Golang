dbf6c8ff6e3dc20c5adb99b9acabb24928f8cd92
tamilarasana <tamilarsanars@gmail.com>
2020-10-22 13:57:40 +0530

    variables

---
+++ variables/variable.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var name string
+var age int
+
+func main() {
+	name = "tamil"
+	age = 24
+	fmt.Println("My name is ", name)
+	fmt.Println("My age is", age)
+	fmt.Printf("I am %s my age is %d", name, age)
+
+}
