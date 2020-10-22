07729aeee48fd8d829236fcadda43700531f65ed
tamilarasana <tamilarsanars@gmail.com>
2020-10-23 00:18:25 +0530

    Array

--- array/array.go
+++ array/array.go
@@ -3,14 +3,22 @@ package main
 import "fmt"
 
 func main()  {
-	arr:= [3]int{4,4,5}
+	arr:= [5]int{4,5,6,7,8}
 	sum :=0
 
 	arr2D :=[2][3]int{{1,2,3},{2,4,5}}
 	fmt.Println(arr2D)
+
 	for i:=0; i<len(arr); i++{
 	sum  +=arr[i]
 	}
 	fmt.Println(sum)
 	
 }
+
+
+
+
+
+
+
