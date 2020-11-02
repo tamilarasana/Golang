31b949c0bde185372ccc097f694a296bfe4efb8e
tamilarasana <tamilarsanars@gmail.com>
2020-11-03 00:45:18 +0530

    Structs

--- structs/structs.go
+++ structs/structs.go
@@ -1,34 +1,66 @@
-
 package main
 
 import "fmt"
 
+type User struct {
+	Name  string
+	Email string
+	Age   int
+}
+
+func main() {
+
+	//First way value decleration structs
+
+	tamil := User{"Tamilarasan", "tamil@gmail.com", 24}
+	fmt.Printf("%v\n", tamil)
+	fmt.Printf("%+v\n", tamil)
+	fmt.Printf("%v\n", tamil.Name)
+
+	//second way decleration structs
+
+	var tam = new(User)
+	tam.Name = "Tamil"
+	tam.Email = "tamil@gmail.com"
+	tam.Age = 24
+
+	fmt.Printf("%v\n", tam)
+	fmt.Printf("%+v", tam)
+	fmt.Printf("%v\n", tam.Email)
+
+	//third way value decelaration in structs
+	var sam = &User{"ShanTamil", "shan@gmail.com", 25}
+	fmt.Printf("%v\n", sam)
+	fmt.Printf("%+v\n", sam)
+	fmt.Printf("%v", sam.Age)
+
+}
 
 //Structs
-type car  struct{
-	model string 
-	topspeed int
-	price int
-	cc  int
+// type car  struct{
+// 	model string
+// 	topspeed int
+// 	price int
+// 	cc  int
 
-}
+// }
 
-func main()  {
-	 var car1 car
-	 car1. model = "tata"
-	 car1.topspeed = 500
-	 car1.price = 100000
-	 car1.cc  	= 1200
+// func main()  {
+// 	 var car1 car
+// 	 car1. model = "tata"
+// 	 car1.topspeed = 500
+// 	 car1.price = 100000
+// 	 car1.cc  	= 1200
 
-	printCar(car1)
-}
+// 	printCar(car1)
+// }
 
-func printCar(car2 car)  {
-	fmt.Println(car2.model)
-	fmt.Println(car2.topspeed)
-	fmt.Println(car2.price)
-	fmt.Println(car2.cc)
-}
+// func printCar(car2 car)  {
+// 	fmt.Println(car2.model)
+// 	fmt.Println(car2.topspeed)
+// 	fmt.Println(car2.price)
+// 	fmt.Println(car2.cc)
+// }
 
 /*func main()  {
 	type car struct{
@@ -60,16 +92,15 @@ func main()  {
 	c1 := circle{2.5,&point{4,5}}
 	fmt.Println("Structs program")
 	fmt.Println(c1.center.x)
-	
-}*/
 
+}*/
 
 /*type vertext  struct{
 	x  int
 	y  int
 }
 func main()  {
-       fmt.Println(vertext{10,20})	
+       fmt.Println(vertext{10,20})
 }*/
 
 /*type point struct{
@@ -82,4 +113,3 @@ func main()  {
 	fmt.Println(tamil.y)     //fmt.Println(tamil.y)
 	fmt.Println(tamil2.y)
 }*/
-
