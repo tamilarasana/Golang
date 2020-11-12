dec8a1adcac163c017a44b7f828dce339a915a60
tamilarasana <tamilarsanars@gmail.com>
2020-11-13 00:36:54 +0530

    initial message

--- print.go
+++ print.go
@@ -4,6 +4,6 @@ import "fmt"
 
 func main()  {
 
-	var out string = fmt.Sprintf("tamil \t %05d is tamil", 45)
+	var out string = fmt.Sprintf("tamilarasan \t %05d is tamil", 45)
 	fmt.Println(out)	
 }
